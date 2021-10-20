4b353fb4e364a6d38b630241d6bfa9b2778661fe
Ryszard Pyka <ryszard@pyka.co.uk>
2021-10-20 20:37:50 +0100

    Add descriptions to the PrintDraw extensions.

--- NumeralDash/Other/Extensions.cs
+++ NumeralDash/Other/Extensions.cs
@@ -9,6 +9,11 @@ namespace NumeralDash.Other
     {
         static char spaceCharAlternative = 'a';
 
+        /// <summary>
+        /// Prints text using THeDrawFont and horizontal alignment specified. Calculates x coordinate.
+        /// </summary>
+        /// <param name="y">Y coordinate of the surface.</param>
+        /// <param name="padding">Amount of regular font characters used as horizontal padding on both sides of the output.</param>
         public static void PrintDraw(this ICellSurface c, int y, string text, TheDrawFont f, HorizontalAlignment alignment, int padding = 0)
         {
             int spaceWidth = f.IsCharacterSupported(' ') ? f.GetCharacter(' ').Width : f.GetCharacter(spaceCharAlternative).Width,
@@ -43,8 +48,6 @@ namespace NumeralDash.Other
                 tempText += currentChar;
             }
 
-            
-
             int x = alignment switch
             {
                 HorizontalAlignment.Center => (printWidth - textLength) / 2,
@@ -55,6 +58,13 @@ namespace NumeralDash.Other
             PrintDraw(c, x + padding, y, tempText, f);
         }
 
+        /// <summary>
+        /// Prints the text using TheDrawFont.
+        /// </summary>
+        /// <param name="x">X coordinate of the surface.</param>
+        /// <param name="y">Y coordinate of the surface.</param>
+        /// <param name="text">Text to print.</param>
+        /// <param name="f">Font to use.</param>
         public static void PrintDraw(this ICellSurface c, int x, int y, string text, TheDrawFont f)
         {
             int xPos = x;
